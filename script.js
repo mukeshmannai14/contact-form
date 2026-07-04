2387e46f07e7b683c37403943e0da30b51c97f90
mukeshmannai14 <mukeshkannamannai04@gmail.com>
2026-07-04 18:59:50 +0530

    update a password in html & js file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var nameError = document.getElementById("name-error");
 var phoneError = document.getElementById("phone-error");
 var emailError = document.getElementById("email-error");
-var messageError = document.getElementById("message-error");
+var passError = document.getElementById("password-error");
 var submitError = document.getElementById("submit-error");
 
 function validateName() {
@@ -51,23 +51,33 @@ function validateEmail(){
    
 }
 
-function validatemessage(){
-       var message =document.getElementById("contact-message").value;
-       var required = 30
-       var left = required-message.length
-       if(left>0){
-        messageError.innerHTML= left+"more character is required"
-        return false
-       }
-       messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
-    return true
+function validatepassword() {
+    var password = document.getElementById("contact-password").value;
+
+    if (password.length == 0) {
+        passError.innerHTML = "Password is required";
+        return false;
+    }
+
+    if (password.length < 8) {
+        passError.innerHTML = "Password must be at least 8 characters";
+        return false;
+    }
+     if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#])[A-Za-z\d@$!%*?&.#]{8,}$/)) {
+        passError.innerHTML = "Use uppercase, lowercase, number & special character";
+        return false;
+    }
+
+    passError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
 }
+
  function  validateform(){
-    if(!validateName()|| !validateEmail() || !validatemessage()|| !validatephone()){
+    if(!validateName()|| !validateEmail() || !validatepassword()|| !validatephone()){
         submitError.style.display="block"
     submitError.innerHTML="please fix this  error to submit"
     setTimeout(function(){submitError.style.display="none"},3000)
     return false
     }
-    alert("form submitted successfully🎉")
+    alert("form submitted successfully🎊🎉")
  }
